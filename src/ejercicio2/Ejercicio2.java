009c19fcfff9897b1a7dee149b35ad29c00e6ea0
estebanfak <estebanfak@gmail.com>
2022-08-31 17:23:35 -0300

    ejercicio 2

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,15 @@
+package ejercicio2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+
+        int precioNeto = 100;
+        double precioFinal = precioMasIVA(precioNeto);
+        System.out.println(precioFinal);
+    }
+
+    private static double precioMasIVA(int precio) {
+        return precio + (precio*0.21);
+    }
+}
