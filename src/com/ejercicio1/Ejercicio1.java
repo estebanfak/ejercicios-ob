1d0503abe4be09f80b24dafb2a413c2937abeb9c
estebanfak <estebanfak@gmail.com>
2022-08-31 14:03:44 -0300

    first commit

---
+++ src/com/ejercicio1/Ejercicio1.java
@@ -0,0 +1,25 @@
+package com.ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String cadena = "Hola mundo";
+        int numeroEntero = 564564564;
+        byte numeroByte = 127;
+        short numeroShort = 32767;
+        long numeroLong = 5646545465238273489L;
+        double numeroDouble = 50.56;
+        float numeroFloat = 65456456456.65F;
+        boolean isBoolean = true;
+        char caracter = 'p';
+
+        System.out.println(cadena);
+        System.out.println(numeroEntero);
+        System.out.println(numeroByte);
+        System.out.println(numeroShort);
+        System.out.println(numeroLong);
+        System.out.println(numeroDouble);
+        System.out.println(numeroFloat);
+        System.out.println(isBoolean);
+        System.out.println(caracter);
+    }
+}
